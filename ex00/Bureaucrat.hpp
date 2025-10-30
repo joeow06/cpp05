7e52d7569c827d342ba8f0f9391501d02ef6a737
joeow06 <joeow06@gmail.com>
2025-10-30 23:58:40 +0800

    added ex00

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jow <jow@student.42kl.edu.my>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/30 02:29:26 by jow               #+#    #+#             */
+/*   Updated: 2025/10/30 02:29:27 by jow              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat {
+    private:
+        const std::string _name;
+        int _grade;
+
+    public:
+        Bureaucrat();
+       ~Bureaucrat();
+       Bureaucrat(const Bureaucrat &other);
+       Bureaucrat& operator=(const Bureaucrat &other);
+
+       Bureaucrat(std::string name, int grade);
+       const std::string getName() const;
+       int getGrade() const;
+//       void setGrade(int grade);
+       void incrementGrade();
+       void decrementGrade();
+
+       class GradeTooHighException : public std::exception
+        {
+           public:
+                virtual const char* what() const throw();
+        } ;
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        } ;
+} ;
+
+std::ostream& operator<<(std::ostream &out, Bureaucrat const &a);
+
+#endif
