f812d1b76f5a78f4e993057f6447e37fabea9898
joeow06 <joeow06@gmail.com>
2025-11-11 21:44:03 +0800

    feat: aded signForm function

--- ex01/Bureaucrat.cpp
+++ ex01/Bureaucrat.cpp
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 Bureaucrat::Bureaucrat() : _name("Default"), _grade(0) {}
 
@@ -59,7 +60,15 @@ void Bureaucrat::decrementGrade()
 
 void Bureaucrat::signForm(Form &form)
 {
-
+	try
+	{
+		form.beSigned(*this);
+		std::cout << this->getName() << " signed " << form.getName() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << this->getName() << " couldn't sign " << form.getName() << " because " << e.what() << std::endl;
+	}
 }
 
 const char* Bureaucrat::GradeTooHighException::what() const throw()
