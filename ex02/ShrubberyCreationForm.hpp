5794e96a2f84fc1f62f0730037040ff1cdd2843e
joeow06 <joeow06@gmail.com>
2025-11-13 21:20:09 +0800

    feat: added ShrubberyCreationForm

--- ex02/ShrubberyCreationForm.hpp
+++ ex02/ShrubberyCreationForm.hpp
@@ -10,3 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SHRUBBERYCREATIONFORM_HPP
+#define SHRUBBERYCREATIONFORM_HPP
+
+# include "AForm.hpp"
+
+class ShrubberyCreationForm : public AForm
+{
+	private:
+		std::string target;
+
+	public:
+		ShrubberyCreationForm();
+		~ShrubberyCreationForm();
+		ShrubberyCreationForm(const ShrubberyCreationForm &other);
+		ShrubberyCreationForm& operator=(const ShrubberyCreationForm &other);
+
+		void create();
+};
+
+#endif
