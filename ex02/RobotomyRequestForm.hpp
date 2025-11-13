9877680134df655b08c866a0875b39ad2ce23fb3
joeow06 <joeow06@gmail.com>
2025-11-13 21:24:38 +0800

    feat: added RobotomyRequestForm

---
+++ ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.hpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jow <jow@student.42kl.edu.my>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/13 21:15:01 by jow               #+#    #+#             */
+/*   Updated: 2025/11/13 21:15:02 by jow              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ROBOTOMYREQUESTFORM_HPP
+#define ROBOTOMYREQUESTFORM_HPP
+
+# include "AForm.hpp"
+
+class RobotomyRequestForm : public AForm
+{
+	public:
+		RobotomyRequestForm();
+		~RobotomyRequestForm();
+		RobotomyRequestForm(const RobotomyRequestForm &other);
+		RobotomyRequestForm& operator=(const RobotomyRequestForm &other);
+} ;
+
+#endif
