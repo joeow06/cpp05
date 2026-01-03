475ca55b9cf45e1e68f9eced61488186739ff184
joeow06 <joeow06@gmail.com>
2026-01-03 16:07:41 +0800

    test: added more tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -12,9 +12,24 @@
 
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
-#include <iostream>
+#include "PresidentialPardonForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include <exception>
 
 int main(void)
 {
-	AForm form;
+	try
+	{
+		AForm* form = new ShrubberyCreationForm();
+		std::cout << form->getName() << std::endl;
+		Bureaucrat joe("Joe", 149);
+		joe.signForm(*form);
+		std::cout << form->getSigned() << std::endl;
+		joe.executeForm(*form);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Exception : " << e.what() << std::endl;
+	}
 }
