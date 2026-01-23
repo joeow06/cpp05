8b24a2c92ef3ab1073719a7201d749e66fae11b4
joeow06 <joeow06@gmail.com>
2026-01-23 15:54:17 +0800

    test: added intern tests

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -15,96 +15,52 @@
 #include "PresidentialPardonForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "Intern.hpp"
 #include <exception>
 
 int main(void)
 {
-	// Test 1
+	// AForm* test;
+	// Intern joe;
+
+	// test = joe.makeForm("PresidentialPardonForm", "LMAO");
+	// (void) test;
 	try
 	{
-		Bureaucrat joe("Joe", 1);
+		Intern internJoe;
+		AForm* form1;
+		AForm* form2;
+		AForm* form3;
+		AForm* badform;
 
-		AForm* form = new ShrubberyCreationForm("Joe's home");
-		joe.signForm(*form);
-		joe.executeForm(*form);
+		form1 = internJoe.makeForm("ShrubberyCreationForm", "ONE");
+		form2 = internJoe.makeForm("RobotomyRequestForm", "TWO");
+		form3 = internJoe.makeForm("PresidentialPardonForm", "THREE");
+		badform = internJoe.makeForm("Sh", "lol");
 		std::cout << std::endl;
 
+		Bureaucrat joe("Joe", 1);
 
-		AForm* form2 = new RobotomyRequestForm("Robot");
+		joe.signForm(*form1);
+		joe.executeForm(*form1);
+		std::cout << std::endl;
+
 		joe.signForm(*form2);
 		joe.executeForm(*form2);
 		std::cout << std::endl;
 
-		AForm* form3 = new PresidentialPardonForm("President");
 		joe.signForm(*form3);
 		joe.executeForm(*form3);
 
-		delete form;
+		delete form1;
 		delete form2;
 		delete form3;
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << "Exception : " << e.what() << std::endl;
-	}
-
-	// Test 2
-	std::cout << "---------------------" << std::endl;
-	try
-	{
-		Bureaucrat randy("Randy", 150);
-
-		AForm* form4 = new ShrubberyCreationForm("Randy's home");
-		randy.signForm(*form4);
-		randy.executeForm(*form4);
-		std::cout << std::endl;
-
-
-		AForm* form5 = new RobotomyRequestForm("Robot");
-		randy.signForm(*form5);
-		randy.executeForm(*form5);
-		std::cout << std::endl;
-
-		AForm* form6 = new PresidentialPardonForm("President");
-		randy.signForm(*form6);
-		randy.executeForm(*form6);
-
-		delete form4;
-		delete form5;
-		delete form6;
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << "Exception : " << e.what() << std::endl;
-	}
-
-	// Test 3
-	std::cout << "---------------------" << std::endl;
-	try
-	{
-		Bureaucrat james("James", 70);
-
-		AForm* form7 = new ShrubberyCreationForm("James's home");
-		james.signForm(*form7);
-		james.executeForm(*form7);
-		std::cout << std::endl;
-
-
-		AForm* form8 = new RobotomyRequestForm("Robot");
-		james.signForm(*form8);
-		james.executeForm(*form8);
-		std::cout << std::endl;
-
-		AForm* form9 = new PresidentialPardonForm("President");
-		james.signForm(*form9);
-		james.executeForm(*form9);
-
-		delete form7;
-		delete form8;
-		delete form9;
+		delete badform;
 	}
 	catch (const std::exception &e)
 	{
 		std::cerr << "Exception : " << e.what() << std::endl;
 	}
 }
+
+// joe.signForm(*badform) will cause segfault because you cant dereference a null pointer
